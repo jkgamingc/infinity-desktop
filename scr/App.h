cff9301e9d706ff78e9542c37cd4efdf4e804b9d
jkgamingc <80155461+jkgamingc@users.noreply.github.com>
2021-07-16 07:09:20 -0700

    Add files via upload

---
+++ scr/App.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class App
+{
+private:
+	int option;
+	bool running;
+
+public:
+	bool isrunning();
+	void downloads();
+	void menu();
+
+};
+
---
+++ scr/main.cpp
@@ -0,0 +1,10 @@
+#include "App.h"
+
+int main() {
+
+	App a;
+
+	a.menu();
+
+	return 0;
+}
